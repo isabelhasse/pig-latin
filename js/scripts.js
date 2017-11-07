724f7ace2ba61d7942cdc75292596190da3e30ca
Emily Watkins and Isabel Hasse <isabel@>
2017-11-06 20:55:31 -0800

    add code to figure out if first letter is a vowel and if so do nothing with first letter and add way.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-var pigLatin = function(word) {
+var vowels = ["a", "e", "i", "o", "u"];
+
+
+var pigLatinVowel = function(word) {
+  return word + "way";
+};
+
+var multipleConsonants = function(word) {
+  var consonants = [];
+  var letters = word.split("");
+  var i = 0;
+  while(!(vowels.includes(letters[i]))) {
+    consonants.push(letters[i]);
+    letters.shift();
+    i++;
+  };
+  return consonants;
+};
+
+var pigLatinConsonant = function(word) {
   var characterArray = word.split("");
   characterArray.push(characterArray[0]);
   characterArray.shift();
@@ -6,13 +25,21 @@ var pigLatin = function(word) {
   return newWord + "ay";
 };
 
+var pigLatin = function(word) {
+  if (word.charAt(0) === "a" || word.charAt(0) === "e" || word.charAt(0) === "i" || word.charAt(0) === "o" || word.charAt(0) === "u") {
+    return pigLatinVowel(word);
+  } else {
+    return pigLatinConsonant(word);
+  }
+};
 
 $(document).ready(function() {
   $("form#piglatin").submit(function(event) {
     event.preventDefault();
     var input = $("#input").val();
-
     var result = pigLatin(input);
+    console.log(multipleConsonants(input));
+
 
     $("#result").text(result);
     $("#result").show();
