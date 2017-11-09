6f1601f4589d6df774719d231f589f5baf11c314
Andrea Wilkowski and Isabel Hasse <andrea@>
2017-11-08 20:56:51 -0800

    deal with words that contain qu and capital letters

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+Completed Specs:
 add "ay" to any word (friend > frienday)
 move first letter to end before "ay" (dog > ogday)
-doesn't do 2 if starts with vowel (ice > iceay)
-move all consonants before first vowel to end before "ay" (glass > assglay)
+don't do 1 or 2 if word starts with a vowel, instead add "way" (ice > iceway)
+move all consonants before first vowel to end before "ay" (string > ingstray)
+move "qu" to end before "ay" (queen > eenquay)
+
+Specs to-do:
+handle non-letters (345!! > 345!!)
+handle combinations of letters and non-letters (dog! > ogday!)
+handle multiple words (hello world > ellohay orldway)
+handle uppercase (HELLO > ELLOHAY)
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
-var vowels = ["a", "e", "i", "o", "u"];
+var vowels = "aeiouAEIOU";
+var numbers = "1234567890"
 
-
+//runs if word starts with a vowel
 var pigLatinVowel = function(word) {
   return word + "way";
 };
 
+// var pigLatinConsonant = function(word) {
+//   var characterArray = word.split("");
+//   characterArray.push(characterArray[0]);
+//   characterArray.shift();
+//   var newWord = characterArray.join("");
+//   return newWord + "ay";
+// };
+
+//runs if word starts with a consonant
 var pigLatinConsonant = function(word) {
-  var consonants = [];
-  var letters = word.split("");
-  while(!(vowels.includes(letters[0]))) {
-    consonants.push(letters[0]);
-    letters.shift();
-  };
-  var newWord = letters.join("")+consonants.join("")+"ay";
+  var consonants = "";
+  while(!(vowels.includes(word.charAt(0)))) {
+    consonants = consonants + word.charAt(0);
+    word = word.substr(1);
+  }; //while first letter is not a vowel, add first letter to the string 'consonants' and remove it from the word
+  var newWord = word+consonants+"ay";
   return newWord;
+};
+
+//runs if word starts with "qu"
+var pigLatinQu = function(word) {
+  word = word.substr(2)+"quay";
+  return word;
 };
 
 var pigLatin = function(word) {
   if (vowels.includes(word.charAt(0))) {
     return pigLatinVowel(word);
+  } else if (word.substr(0,2) === "qu"){
+    return pigLatinQu(word);
   } else {
     return pigLatinConsonant(word);
   }
@@ -26,11 +43,10 @@ var pigLatin = function(word) {
 
 $(document).ready(function() {
   $("form#piglatin").submit(function(event) {
+    debugger;
     event.preventDefault();
     var input = $("#input").val();
     var result = pigLatin(input);
-    console.log(multipleConsonants(input));
-
 
     $("#result").text(result);
     $("#result").show();
